508340f2cdceec19b785b08897ee21e757cb855f
Рамиз <ramiz.ilyazov@mail.ru>
2022-09-18 13:30:09 +0500

    second  commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+string[] arrayA = new string[] {"23","pip","cat","moscow","-)34"};
+string[] arrayB = new string[arrayA.Length];
+
+void NewArray(string[] arrayA, string[] arrayB )
+{   
+    int count = 0;
+
+   for(int i = 0; i < arrayA.Length; i++)
+   {
+        if(arrayA[i].Length <= 3);
+        
